5ab1a42fe15e66d25390f171f2ffaac80d66dd37
notebyte7 <notebyte@yandex.ru>
2023-06-16 21:48:20 +0300

    fix: ТЗ16

--- gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
+++ gateway/src/main/java/ru/practicum/shareit/booking/BookingController.java
@@ -1,62 +1,51 @@
 package ru.practicum.shareit.booking;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
-import ru.practicum.shareit.booking.dto.BookItemRequestDto;
 import ru.practicum.shareit.booking.dto.BookingDto;
-import ru.practicum.shareit.booking.dto.BookingState;
-
-import javax.validation.Valid;
-import javax.validation.constraints.Positive;
-import javax.validation.constraints.PositiveOrZero;
-import java.util.List;
 
 @RestController
 @RequestMapping(path = "/bookings")
 @RequiredArgsConstructor
 @Validated
 public class BookingController {
-	private final BookingClient bookingClient;
+    private final BookingClient bookingClient;
 
-	@PostMapping
-	@ResponseBody
-	ResponseEntity<Object> createBooking(@RequestHeader("X-Sharer-User-Id") int bookerId, @RequestBody BookingDto bookingDto) {
-		return bookingClient.createBooking(bookerId, bookingDto);
-	}
+    @PostMapping
+    @ResponseBody
+    ResponseEntity<Object> createBooking(@RequestHeader("X-Sharer-User-Id") int bookerId, @RequestBody BookingDto bookingDto) {
+        return bookingClient.createBooking(bookerId, bookingDto);
+    }
 
-	@PatchMapping("/{bookingId}")
-	@ResponseBody
-	ResponseEntity<Object> approvingBooking(@RequestHeader("X-Sharer-User-Id") long userId, @PathVariable long bookingId,
-									  @RequestParam boolean approved) {
-		return bookingClient.approvingBooking(userId, bookingId, approved);
-	}
+    @PatchMapping("/{bookingId}")
+    @ResponseBody
+    ResponseEntity<Object> approvingBooking(@RequestHeader("X-Sharer-User-Id") long userId, @PathVariable long bookingId,
+                                            @RequestParam boolean approved) {
+        return bookingClient.approvingBooking(userId, bookingId, approved);
+    }
 
-	@GetMapping("/{bookingId}")
-	@ResponseBody
-	ResponseEntity<Object> getBookingById(@RequestHeader("X-Sharer-User-Id") long userId, @PathVariable Long bookingId) {
-		return bookingClient.getBookingById(userId, bookingId);
-	}
+    @GetMapping("/{bookingId}")
+    @ResponseBody
+    ResponseEntity<Object> getBookingById(@RequestHeader("X-Sharer-User-Id") long userId, @PathVariable Long bookingId) {
+        return bookingClient.getBookingById(userId, bookingId);
+    }
 
-	@GetMapping()
-	@ResponseBody
-	ResponseEntity<Object> getBookingByUser(@RequestHeader(value = "X-Sharer-User-Id", required = false) long userId,
-											@RequestParam(defaultValue = "ALL", required = false) String state,
-											@RequestParam(required = false) Integer from, Integer size) {
-		return bookingClient.getBookingByUser(userId, state, from, size);
-	}
+    @GetMapping()
+    @ResponseBody
+    ResponseEntity<Object> getBookingByUser(@RequestHeader(value = "X-Sharer-User-Id", required = false) long userId,
+                                            @RequestParam(defaultValue = "ALL", required = false) String state,
+                                            @RequestParam(required = false) Integer from, Integer size) {
+        return bookingClient.getBookingByUser(userId, state, from, size);
+    }
 
-	@GetMapping("/owner")
-	@ResponseBody
-	ResponseEntity<Object> getBookingByOwner(@RequestHeader(value = "X-Sharer-User-Id", required = false) long userId,
-											 @RequestParam(defaultValue = "ALL", required = false) String state,
-											 @RequestParam(required = false) Integer from, Integer size) {
+    @GetMapping("/owner")
+    @ResponseBody
+    ResponseEntity<Object> getBookingByOwner(@RequestHeader(value = "X-Sharer-User-Id", required = false) long userId,
+                                             @RequestParam(defaultValue = "ALL", required = false) String state,
+                                             @RequestParam(required = false) Integer from, Integer size) {
 
-		return bookingClient.getBookingByOwner(userId, state, from, size);
-	}
+        return bookingClient.getBookingByOwner(userId, state, from, size);
+    }
 }
--- gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
+++ gateway/src/main/java/ru/practicum/shareit/item/ItemController.java
@@ -5,7 +5,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.shareit.item.dto.CommentDto;
 import ru.practicum.shareit.item.dto.ItemDto;
-import ru.practicum.shareit.item.ItemClient;
 
 import javax.validation.Valid;
 
@@ -18,42 +17,42 @@ public class ItemController {
 
     @GetMapping
     public ResponseEntity<Object> getItemsByOwner(@RequestHeader(USER_ID_HEADER) Long ownerId,
-                                                   @RequestParam(defaultValue = "0", required = false) Integer from,
-                                                   @RequestParam(defaultValue = "10", required = false) Integer size) {
+                                                  @RequestParam(defaultValue = "0", required = false) Integer from,
+                                                  @RequestParam(defaultValue = "10", required = false) Integer size) {
         return itemClient.getItemsByOwner(ownerId, from, size);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Object> getItemDtoById(@RequestHeader(USER_ID_HEADER) Long userId,
-                               @PathVariable Long id) {
+                                                 @PathVariable Long id) {
         return itemClient.getItemDtoById(userId, id);
     }
 
     @GetMapping("/search")
     public ResponseEntity<Object> searchItemsByText(@RequestHeader(USER_ID_HEADER) Long userId,
-                                   @RequestParam("text") String text,
-                                   @RequestParam(defaultValue = "0", required = false) Integer from,
-                                   @RequestParam(defaultValue = "10", required = false) Integer size) {
+                                                    @RequestParam("text") String text,
+                                                    @RequestParam(defaultValue = "0", required = false) Integer from,
+                                                    @RequestParam(defaultValue = "10", required = false) Integer size) {
         return itemClient.searchItemsByText(userId, text, from, size);
     }
 
     @PostMapping
     public ResponseEntity<Object> createItem(@RequestHeader(USER_ID_HEADER) Long ownerId,
-                            @Valid @RequestBody ItemDto itemDto) {
+                                             @Valid @RequestBody ItemDto itemDto) {
         return itemClient.createItem(ownerId, itemDto);
     }
 
     @PostMapping("/{id}/comment")
     public ResponseEntity<Object> createComment(@RequestHeader(USER_ID_HEADER) Long userId,
-                                  @PathVariable Long id,
-                                  @Valid @RequestBody CommentDto commentDto) {
+                                                @PathVariable Long id,
+                                                @Valid @RequestBody CommentDto commentDto) {
         return itemClient.createComment(userId, id, commentDto);
     }
 
     @PatchMapping("/{id}")
     public ResponseEntity<Object> updateItem(@RequestHeader(USER_ID_HEADER) Long ownerId,
-                             @PathVariable Long id,
-                             @RequestBody String json) {
+                                             @PathVariable Long id,
+                                             @RequestBody String json) {
         return itemClient.updateItem(ownerId, id, json);
     }
 
